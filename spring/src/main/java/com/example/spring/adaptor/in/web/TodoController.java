3ea8d3d6adf43c7421246fca9743eb64b5f02b1a
이가은 <gaeun.lee@mirero.co.kr>
2024-02-27 12:42:24 +0900

    cors랑 delete해결

--- spring/src/main/java/com/example/spring/adaptor/in/web/TodoController.java
+++ spring/src/main/java/com/example/spring/adaptor/in/web/TodoController.java
@@ -7,11 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class TodoController {
@@ -34,8 +32,8 @@ public class TodoController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping("/todoList")
-    public ResponseEntity<Void> deleteTodo(@RequestParam Long id) {
+    @DeleteMapping("/todoList/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable("id") Long id) {
         todoUseCase.delete(id);
         return ResponseEntity.ok().build();
     }
