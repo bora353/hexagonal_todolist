040c95152b6fab37609dabafdd1e7505fab9500e
이가은 <gaeun.lee@mirero.co.kr>
2024-02-27 14:47:58 +0900

    env로 설정

--- spring/src/main/resources/data.sql
+++ spring/src/main/resources/data.sql
@@ -1,5 +1,11 @@
-INSERT INTO tb_todo (id, content, name, description, complete)
-VALUES (1, '공부하기', '사람1', '설명1', false);
+-- INSERT INTO tb_todo (id, content, name, description, complete)
+-- VALUES (1, '공부하기', '사람1', '설명1', false);
+--
+-- INSERT INTO tb_todo (id, content, name, description, complete)
+-- VALUES (2, '밥먹기', '사람2', '설명2', false);
 
-INSERT INTO tb_todo (id, content, name, description, complete)
-VALUES (2, '밥먹기', '사람2', '설명2', false);
+INSERT INTO tb_todo (content, name, description, complete)
+VALUES ('공부하기', '사람1', '설명1', false);
+
+INSERT INTO tb_todo (content, name, description, complete)
+VALUES ('밥먹기', '사람2', '설명2', false);
