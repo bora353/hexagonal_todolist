fb25c45e7c63be3b75816a8a99a0a59920dd92ac
이가은 <gaeun.lee@mirero.co.kr>
2024-02-29 08:27:36 +0900

    controller env 테스트

--- spring/src/main/java/com/example/spring/adaptor/in/web/TodoController.java
+++ spring/src/main/java/com/example/spring/adaptor/in/web/TodoController.java
@@ -3,6 +3,11 @@ package com.example.spring.adaptor.in.web;
 import com.example.spring.application.port.in.TodoUseCase;
 import com.example.spring.domain.Todo;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +17,18 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Slf4j
 public class TodoController {
 
     private final TodoUseCase todoUseCase;
 
-    @GetMapping("/")
-    public String index() {
-        return "spring!";
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/abc")
+    public String[] index() {
+        log.info(environment.getProperty("spring.datasource.url"));
+        return environment.getActiveProfiles();
     }
 
     @GetMapping("/todoList")
